1acad5ea7ab18a05cab4e0f350896d00594c3726
matthegg5 <matthew.heggie@googlemail.com>
2023-05-03 17:58:54 +0100

    test commit

--- src/main.cpp
+++ src/main.cpp
@@ -114,6 +114,7 @@ int main(int, char**) {
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0); 
+    //test push
 
     // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
     //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
